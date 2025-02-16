e651122df69cf5ab75e803d827365ebe9086ad09
killermaxper01 <studyonlyjnv@gmail.com>
2025-02-16 20:49:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,4 +67,4 @@ def ratelimit_exceeded(e):
 
 # ✅ Fix Deployment: Run on 0.0.0.0 (Public Access)
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0",port=port) #port=10000
